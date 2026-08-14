70d63f886c2d9e65f6ab8daeb43afbbb13622aa1
Shivam <mehtashivam361@gmail.com>
2026-08-14 05:51:26 +0530

    Move Terraform state to S3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "tfstate-intent-guard-shivam-27087"
+    key    = "infra/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
 }
 
 provider "aws" {
